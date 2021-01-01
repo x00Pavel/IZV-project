dfc04fe5c4eda95b3893094c07cd75ca38c840ca
x00Pavel <pavel.yadlouski@gmail.com>
2021-01-01 22:55:38 +0100

    add last section

---
+++ doc.py
@@ -0,0 +1,13 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+import contextily as ctx
+import numpy as np
+
+
+def load_df(file_name: str):
+    pass
+
+
+
+if __name__ == "__main__":
+    data = load_df("accedent.pkl.gz")
